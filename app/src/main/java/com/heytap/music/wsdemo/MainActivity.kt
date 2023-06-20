9fc505f38b34fa31f419897f2ded1a82b0e24a77
munna.kumar <munna.kumar@hungama.com>
2023-06-20 18:50:25 +0530

    firebase json changes

--- app/src/main/java/com/heytap/music/wsdemo/MainActivity.kt
+++ app/src/main/java/com/heytap/music/wsdemo/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        CommonUtillls.getFirebaseConfigAdsData()
         startActivity(Intent(this, SplashActivity::class.java))
     }
 }
