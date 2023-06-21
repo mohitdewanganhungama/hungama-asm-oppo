da85117ab469d81a072c35b05a5608b5c47f2996
munna.kumar <munna.kumar@hungama.com>
2023-06-21 11:53:58 +0530

    initialize firebase configue in MainActivity

--- app/src/main/java/com/heytap/music/wsdemo/MainActivity.kt
+++ app/src/main/java/com/heytap/music/wsdemo/MainActivity.kt
@@ -3,12 +3,32 @@ package com.heytap.music.wsdemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.hungama.music.data.model.SongDurationConfigModel
 import com.hungama.music.ui.main.view.activity.SplashActivity
+import kotlinx.coroutines.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, SplashActivity::class.java))
+        FirebaseApp.initializeApp(this)
+        try {
+            CoroutineScope(Dispatchers.IO).launch{
+                Firebase.remoteConfig.fetchAndActivate()
+                startActivity(Intent(this@MainActivity, SplashActivity::class.java))
+            }
+        }catch (e:Exception){
+        }
+    }
+
+    fun getFirebaseData():Unit = runBlocking(Dispatchers.IO){
+        val remoteConfig = Firebase.remoteConfig
+        val nudge_stream_preview = remoteConfig.getString("nudge_stream_preview")
+        Log.v("haohgdsa", nudge_stream_preview)
     }
 }
