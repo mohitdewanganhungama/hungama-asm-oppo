9495d7133eceb25ec5767ec70758d25877e44813
AndroidDev789 <135225213+AndroidPoulami@users.noreply.github.com>
2023-09-11 15:01:37 +0530

    banner api endpoint change

---
+++ mylibrary/build/tmp/kapt3/stubs/debug/com/hungama/music/eventanalytic/eventreporter/Hero_card_viewed.java
@@ -0,0 +1,37 @@
+package com.hungama.music.eventanalytic.eventreporter;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B)\u0012\"\u0010\u0002\u001a\u001e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u00040\u0003j\u000e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u0004`\u0005\u00a2\u0006\u0002\u0010\u0006J\b\u0010\t\u001a\u00020\u0004H\u0016J$\u0010\n\u001a\u001e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u00040\u0003j\u000e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u0004`\u0005H\u0016J\b\u0010\u000b\u001a\u00020\fH\u0016R-\u0010\u0002\u001a\u001e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u00040\u0003j\u000e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u0004`\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\b\u00a8\u0006\r"}, d2 = {"Lcom/hungama/music/eventanalytic/eventreporter/Hero_card_viewed;", "Lcom/hungama/music/eventanalytic/event/IEvent;", "hashMap", "Ljava/util/HashMap;", "", "Lkotlin/collections/HashMap;", "(Ljava/util/HashMap;)V", "getHashMap", "()Ljava/util/HashMap;", "getName", "getProperty", "getType", "Lcom/hungama/music/eventanalytic/event/EventType;", "mylibrary_debug"})
+public final class Hero_card_viewed implements com.hungama.music.eventanalytic.event.IEvent {
+    @org.jetbrains.annotations.NotNull
+    private final java.util.HashMap<java.lang.String, java.lang.String> hashMap = null;
+    
+    public Hero_card_viewed(@org.jetbrains.annotations.NotNull
+    java.util.HashMap<java.lang.String, java.lang.String> hashMap) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final java.util.HashMap<java.lang.String, java.lang.String> getHashMap() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    @java.lang.Override
+    public java.lang.String getName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    @java.lang.Override
+    public com.hungama.music.eventanalytic.event.EventType getType() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    @java.lang.Override
+    public java.util.HashMap<java.lang.String, java.lang.String> getProperty() {
+        return null;
+    }
+}
