ca9b3c48b710da6842fa34470c54eb70a255e5eb
AndroidDev789 <135225213+AndroidPoulami@users.noreply.github.com>
2023-09-12 11:05:51 +0530

    Revert "Merge branch 'main' of https://github.com/mohitdewanganhungama/hungama-asm-oppo"
    This reverts commit c9a66282560e2d1fc2da20ec2ec026abb1c254f0, reversing
    changes made to d465d3726f1fe64bb1579ececb6fffde46be034d.

--- mylibrary/build/generated/source/kapt/debug/com/hungama/music/databinding/RowItype50BindingImpl.java
+++ mylibrary/build/generated/source/kapt/debug/com/hungama/music/databinding/RowItype50BindingImpl.java
@@ -14,20 +14,13 @@ public class RowItype50BindingImpl extends RowItype50Binding  {
     static {
         sIncludes = null;
         sViewsWithIds = new android.util.SparseIntArray();
-        sViewsWithIds.put(R.id.episode_player_view, 1);
-        sViewsWithIds.put(R.id.vTopBottom, 2);
-        sViewsWithIds.put(R.id.preViewProgressBar, 3);
-        sViewsWithIds.put(R.id.llMute, 4);
-        sViewsWithIds.put(R.id.ivMuteUnmute, 5);
-        sViewsWithIds.put(R.id.ivUserImage, 6);
-        sViewsWithIds.put(R.id.llBottom, 7);
-        sViewsWithIds.put(R.id.tvTitle, 8);
-        sViewsWithIds.put(R.id.tvSubTitle, 9);
-        sViewsWithIds.put(R.id.llRent, 10);
-        sViewsWithIds.put(R.id.ivRent, 11);
-        sViewsWithIds.put(R.id.txtRent, 12);
-        sViewsWithIds.put(R.id.rlAction, 13);
-        sViewsWithIds.put(R.id.ivAction, 14);
+        sViewsWithIds.put(R.id.ivUserImage, 1);
+        sViewsWithIds.put(R.id.vCenterGradient, 2);
+        sViewsWithIds.put(R.id.tvTitle, 3);
+        sViewsWithIds.put(R.id.tvSubTitle, 4);
+        sViewsWithIds.put(R.id.llRent, 5);
+        sViewsWithIds.put(R.id.ivRent, 6);
+        sViewsWithIds.put(R.id.txtRent, 7);
     }
     // views
     // variables
@@ -36,24 +29,17 @@ public class RowItype50BindingImpl extends RowItype50Binding  {
     // Inverse Binding Event Handlers
 
     public RowItype50BindingImpl(@Nullable androidx.databinding.DataBindingComponent bindingComponent, @NonNull View root) {
-        this(bindingComponent, root, mapBindings(bindingComponent, root, 15, sIncludes, sViewsWithIds));
+        this(bindingComponent, root, mapBindings(bindingComponent, root, 8, sIncludes, sViewsWithIds));
     }
     private RowItype50BindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
         super(bindingComponent, root, 0
-            , (androidx.media3.ui.PlayerView) bindings[1]
-            , (com.hungama.music.utils.customview.fontview.FontAwesomeImageView) bindings[14]
-            , (com.hungama.music.utils.customview.fontview.FontAwesomeImageView) bindings[5]
-            , (com.hungama.music.utils.customview.fontview.FontAwesomeImageView) bindings[11]
-            , (com.google.android.material.imageview.ShapeableImageView) bindings[6]
-            , (androidx.appcompat.widget.LinearLayoutCompat) bindings[7]
+            , (com.hungama.music.utils.customview.fontview.FontAwesomeImageView) bindings[6]
+            , (com.google.android.material.imageview.ShapeableImageView) bindings[1]
             , (androidx.constraintlayout.widget.ConstraintLayout) bindings[0]
-            , (androidx.appcompat.widget.LinearLayoutCompat) bindings[4]
-            , (androidx.appcompat.widget.LinearLayoutCompat) bindings[10]
-            , (android.widget.ProgressBar) bindings[3]
-            , (android.widget.RelativeLayout) bindings[13]
-            , (androidx.appcompat.widget.AppCompatTextView) bindings[9]
-            , (androidx.appcompat.widget.AppCompatTextView) bindings[8]
-            , (android.widget.TextView) bindings[12]
+            , (androidx.appcompat.widget.LinearLayoutCompat) bindings[5]
+            , (androidx.appcompat.widget.AppCompatTextView) bindings[4]
+            , (androidx.appcompat.widget.AppCompatTextView) bindings[3]
+            , (android.widget.TextView) bindings[7]
             , (android.view.View) bindings[2]
             );
         this.llMain.setTag(null);
